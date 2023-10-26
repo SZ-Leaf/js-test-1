1c506be1596e900970e57cd735da70e830bd1cfe
Sary Zeaiter <saryzeaiter@gmail.com>
2023-10-26 17:02:49 +0200

    commit-1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var scene = document.querySelector('.scene');
 var cards = document.querySelectorAll('.card');
 
 for(let i =0; i<cards.length; i++){
-   cards[i].children[0].textContent = Math.round((i +1)/2)
+   // cards[i].children[0].textContent = Math.round((i +1)/2)
    cards[i].children[1].textContent = i
    cards[i].addEventListener('click', onCardClick)
 }
